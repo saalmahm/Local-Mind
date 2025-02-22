1d029b98d32b9c5290fe925213a2a88ccb248d66
salma hamdi <saalmahm51@gmail.com>
2025-02-22 17:55:47 +0100

    Ajouter la section 'mes questions'

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,19 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\CommentController;
-
-
 Route::middleware('auth')->group(function () {
 
     Route::get('/', function () {
         return redirect()->route('questions.index');
     });
-    
-    Route::resource('questions', QuestionController::class);
-    Route::get('/my-questions', [QuestionController::class, 'myQuestions'])->name('questions.my');
 
-    Route::get('/questions', [QuestionController::class, 'index'])->name('questions.index');
-    Route::get('/questions/create', [QuestionController::class, 'create'])->name('questions.create');
-    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
-    Route::get('/questions/{question}', [QuestionController::class, 'show'])->name('questions.show');
-    Route::get('/questions/{question}/edit', [QuestionController::class, 'edit'])->name('questions.edit');
-    Route::put('/questions/{question}', [QuestionController::class, 'update'])->name('questions.update');
-    Route::delete('/questions/{question}', [QuestionController::class, 'destroy'])->name('questions.destroy');
+    Route::get('questions/my', [QuestionController::class, 'myQuestions'])->name('questions.my');
+
+    Route::resource('questions', QuestionController::class)->except(['show']);
+
+    Route::get('/questions/{question}', [QuestionController::class, 'show'])
+        ->where('question', '[0-9]+') // Enforce only numeric IDs
+        ->name('questions.show');
 
     // Comment routes
     Route::post('/questions/{question}/comments', [CommentController::class, 'store'])->name('comments.store');
@@ -32,5 +27,4 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 require __DIR__.'/auth.php';
